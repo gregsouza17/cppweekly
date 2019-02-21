ed2aabb6aee8d8abc80f0726b6ee1fb586d86d00
myName <myemail.mail>
2019-02-21 08:57:41 -0300

    Quizz 11

--- cpt11greg/classesGame.cpp
+++ cpt11greg/classesGame.cpp
@@ -4,6 +4,16 @@
 #include <iostream>
 #include <string>
 
+
+
+int getRandomNumber(int min, int max)
+{
+  static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+  // sttatic used for efficiency, so we only calculate this value once
+  // evenly distribute the random number across our range
+  return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
 class Creature {
 
 //  The Creature class will consist of: (string) Name,
@@ -32,10 +42,12 @@ public:
   int getGold() {return m_gold;}
 
   void reduceHealth(int value){
+    //Reduce a Creature Health by Value
     m_health-=value;
   }
 
   bool isDead(){
+    //Returns true if heal is lower than 0
     return m_health<=0;
   }
 
@@ -46,6 +58,9 @@ public:
 };
 
 class Player: public Creature{
+  /* The Player class inehirits from Creature, it has an addiotinal level
+member, with a getter, and a levelUp method, and a hasWon method
+*/
 private:
   int m_level;
 public:
@@ -54,23 +69,26 @@ public:
   int getLevel() {return m_level;}
   
   void levelUp(){
+    //When the player level up, he gets +1 Damage and Level
     m_damage+=1;
     m_level+=1;
   }
 
   bool hasWon(){
-    return m_level>19;
+    return m_level==19;
   }
   
 };
 
 class Monster: public Creature{
+  /*The monster class will have associeted with it an Enum with
+all possible monster types, a static member with the data about the monster.*/
 public:
   enum Type{
-    DRAGON,
-    ORC,
-    SLIME,
-    MAX_TYPE
+    DRAGON, // 0
+    ORC, // 1 
+    SLIME, // 2
+    MAX_TYPE // 3
   };
 
   struct MonsterData{
@@ -81,12 +99,26 @@ public:
 
   static MonsterData monsterData[MAX_TYPE];
 
-  Monster(Type type): Creature(monsterData[type].name, monsterData[type].symbol, monsterData[type].health,
-			       monsterData[type].damage, monsterData[type].gold)
+
+  //The monster constructor gets the creature Type, and uses the static monsterData to get
+  //the Monster damage, name, symbol...
+  Monster(Type type): Creature(monsterData[type].name, monsterData[type].symbol,
+			       monsterData[type].health, monsterData[type].damage,
+			       monsterData[type].gold)
   {
-    
   }
-  
+
+
+  static Monster getRandomMonster(){
+    //Returns a random monster
+    
+    int r = getRandomNumber(0, MAX_TYPE-1);
+
+    Monster m( static_cast<Type>(r));
+    
+    return m;
+  }
+
 };
 
 
--- cpt11greg/game.cpp
+++ cpt11greg/game.cpp
@@ -2,12 +2,102 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib> // for rand() and srand()
+#include <ctime> // for time()
+ 
+// Generate  random number between min and max (inclusive)
+// Assumes srand() has already been called
 
+void attackMonster(Monster &m, Player &p);
+void attackPlayer(Monster &m, Player &p);
 
-int main()
-{
-  Monster m(Monster::ORC);
-  std::cout << "A " << m.getName() << " (" << m.getSymbol() << ") was created.\n";
+void fightMonster(Player &p);
+
+int main(){
+
+  srand(static_cast<unsigned int>(time(0)));
+  // set initial seed value to system clock
+  rand(); // get rid of first result
+
+  std::cout<<"Enter Your Name: ";
+  std::string name;
+  std::cin>>name;
+
+  
+  Player p(name);
+  std::cout << "Welcome" << p.getName() << "\n";
+
+  while(!p.isDead() && !p.hasWon())
+    fightMonster(p);
+
+  if (p.isDead())
+    {
+      std::cout << "You died at level " << p.getLevel() << " and with " << p.getGold() << " gold.\n";
+      std::cout << "Too bad you can't take it with you!\n";
+    }
+  else
+    {
+      std::cout << "You won the game with " << p.getGold() << " gold!\n";
+    }
   
   return 0;
+  
+}
+
+
+void attackMonster(Monster &m, Player &p){
+
+  if(p.isDead()){
+    std::cout << "You Cant Attack, You are Dead! \n";
+    return;
+  }
+
+  m.reduceHealth(p.getDamage());
+  std::cout << "Player " << p.getName() << " attacks creature " << m.getName()<<
+    " for " << p.getDamage() << " damage \n";
+
+  return;
+}
+  
+void attackPlayer(Monster &m, Player &p){
+  if(m.isDead()){
+    std::cout << "The Monster Can't Attack, it is dead";
+    return;
+  }
+
+  p.reduceHealth(m.getDamage());
+  std::cout << "The " << m.getName() << " hit you for " <<
+    m.getDamage() << " damage \n";
+
+  return;
+}
+
+
+
+void fightMonster(Player &p){
+  Monster m = Monster::getRandomMonster();
+  std::cout<<"You encounter monster " << m.getName() << "\n";
+  while(!p.isDead() && !m.isDead()){
+
+    std::cout << "Do you want to (R)un or (F)ight? " << "\n";
+    char c;
+    std::cin >> c;
+
+    if(c=='R' || c=='r'){
+
+      if(getRandomNumber(0,1)==1){
+	std::cout<< "You Flee Sucessfully" << "\n";
+	return;
+      }
+      else{
+	std::cout<< "You Fail to Flee Sucessfully" << "\n";
+	attackPlayer(m,p);
+	continue;
+      }
+    }
+    if(c=='F' || c=='f'){
+      attackMonster(m, p);
+      attackPlayer(m, p);   
+    }
+  }
 }
